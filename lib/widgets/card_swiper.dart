ad5b32a2c4bf4fe420e1f50d5b44c598c1eebfed
David Enamorado <david8enaceituno@gmail.com>
2022-02-03 23:49:22 -0600

    Se agrego el carrusel de tarjetas

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiperScreen extends StatelessWidget {
@@ -5,10 +6,35 @@ class CardSwiperScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Container(
       width: double.infinity,
-      height: 500,
-      color: Colors.indigo,
+      height: size.height * 0.5,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.4,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, 'details');
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: const AssetImage('assets/img/no-image.jpg'),
+                image: NetworkImage(
+                  'https://picsum.photos/id/$index/300/400',
+                ),
+                fadeInDuration: const Duration(milliseconds: 200),
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
