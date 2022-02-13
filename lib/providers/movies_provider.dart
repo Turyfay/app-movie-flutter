5a8a6ab8de196df815bfedb0ddfec77567bfb888
David Enamorado <david8enaceituno@gmail.com>
2022-02-13 01:33:07 -0600

    Se agrego la peticion de peliculas populares

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -9,8 +9,11 @@ class MoviesProvider extends ChangeNotifier {
 
   List<Movie> onDiplaymovies = [];
 
+  List<Movie> onDiplayPopularmovies = [];
+
   MoviesProvider() {
     print('MoviesProvider init');
+    getOnDisplayMovies();
     getOnDisplayMovies();
   }
 
@@ -23,6 +26,21 @@ class MoviesProvider extends ChangeNotifier {
     final response = await http.get(url);
     final moviesResponse = NowPlayingResponse.fromJson(response.body);
     onDiplaymovies = moviesResponse.results;
+    notifyListeners();
+  }
+
+  getPopularMovies() async {
+    var url = Uri.https(_baseUrl, '/3/movie/popular', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1',
+    });
+    final response = await http.get(url);
+    final moviesPopularResponse = PopularResponse.fromJson(response.body);
+    onDiplayPopularmovies = [
+      ...onDiplayPopularmovies,
+      ...moviesPopularResponse.results
+    ];
     notifyListeners();
   }
 }
